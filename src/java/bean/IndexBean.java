824fe699c3b19d1d01a6856621d0930436daf062
killer7tercero <killer7tercero@ainkrad>
2018-08-27 13:35:20 -0600

    Solucionado Bug de grabaciones de indexbean

--- src/java/bean/IndexBean.java
+++ src/java/bean/IndexBean.java
@@ -220,7 +220,7 @@ public class IndexBean implements Serializable {
         sesionUBean s = fc.getApplication().evaluateExpressionGet(fc, "#{sesionUBean}", sesionUBean.class);
         Usuario userAux = s.getUsuario();
         ConferenciaDao daoConferencia = new ConferenciaDao();
-        if(userAux == null){
+        if(userAux == null || userAux.getIdUsuario() == null){
             listaGrabaciones = daoConferencia.obtenerGrabaciones();
         }else{
             if(userAux.getIdRol().getIdRol() == 1){
